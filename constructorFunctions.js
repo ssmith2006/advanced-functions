5fe82927c0fddcc7a4d630abb21759d8a57d68ad
ssmith2006 <shantel.smith@hotmail.com>
2025-02-23 23:52:49 +0000

    setup files, added boilerplates, completed assignment 1

---
+++ constructorFunctions.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function Person(name, age, email) {
+  this.name = name;
+  this.age = age;
+  this.email = email;
+}
+
+ const person1 = new Person ("Shantel", "43", "ssmith2006@gmail.com");
+
+console.log(person1);
