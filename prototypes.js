b027a0319f64422517fa20e73078e56ed5df818e
ssmith2006 <shantel.smith@hotmail.com>
2025-02-24 01:28:49 +0000

    completed assignment 3

--- prototypes.js
+++ prototypes.js
@@ -6,12 +6,9 @@ function Animal(name, species, location) {
   this.species = species;
   this.location = location;
 }
-
- Animal.prototype.sayMoo = function(){
+const cow = new Animal("Connie", "Cow", "Farm");
+ 
+Animal.prototype.sayMoo = function(){
     console.log(this.name + " the " + this.species + " says Moo " + " on the " + this.location);
 };
-const cow = new animal("Connie", "Cow", "Farm");
-
-
-
 cow.sayMoo();
